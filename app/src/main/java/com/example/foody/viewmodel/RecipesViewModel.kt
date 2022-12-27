5bde965e1fb29abd4f0ba591d207c271abb5eadc
CuXuanThoai <92687973+CuXuanThoai@users.noreply.github.com>
2022-12-27 14:17:49 +0700

    first commit

---
+++ app/src/main/java/com/example/foody/viewmodel/RecipesViewModel.kt
@@ -0,0 +1,66 @@
+package com.example.foody.model
+
+import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foody.data.DataStoreRepository
+import com.example.foody.util.Constants.Companion.API_KEY
+import com.example.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.foody.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
+import com.example.foody.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
+import com.example.foody.util.Constants.Companion.QUERY_API_KEY
+import com.example.foody.util.Constants.Companion.QUERY_DIET
+import com.example.foody.util.Constants.Companion.QUERY_FILL_INGREDIENTS
+import com.example.foody.util.Constants.Companion.QUERY_NUMBER
+import com.example.foody.util.Constants.Companion.QUERY_SEARCH
+import com.example.foody.util.Constants.Companion.QUERY_TYPE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class RecipesViewModel @ViewModelInject constructor(application: Application,
+private val dataStoreRepository: DataStoreRepository) :AndroidViewModel(application){
+
+    private  var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+    val readMealDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealDietType (mealType :String,mealTypeId:Int,dietType :String,dietTypeId :Int)=
+        viewModelScope.launch(Dispatchers.IO){
+            dataStoreRepository.saveMealAndDiet(mealType,mealTypeId,dietType,dietTypeId)
+        }
+
+
+    fun applyQueries (): HashMap<String,String>{
+
+        val queries :HashMap<String,String> = HashMap()
+        viewModelScope.launch {
+            readMealDietType.collect { value ->
+                mealType = value.selectedMeal
+                dietType = value.selectedDiet
+            }
+        }
+        queries[QUERY_NUMBER] = "50"
+        queries[QUERY_API_KEY] = API_KEY
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
+        queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
+        queries[QUERY_FILL_INGREDIENTS] = "true"
+        return queries
+    }
+
+    fun applySearchWuery(searchQuey :String) :HashMap<String,String>{
+        val queries : HashMap<String,String> = HashMap()
+        queries[QUERY_SEARCH] = searchQuey
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
+        queries[QUERY_API_KEY] = API_KEY
+        queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
+        queries[QUERY_FILL_INGREDIENTS] = "true"
+
+        return queries
+
+
+    }
+}
